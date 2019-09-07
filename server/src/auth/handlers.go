da7c9426d47465214739fb3ded30b9f9eca73b4d
Gergo <halmi.gergo@gmail.com>
2019-09-07 10:57:03 +0200

    create session cookie on sign up

--- server/src/auth/handlers.go
+++ server/src/auth/handlers.go
@@ -13,18 +13,7 @@ func Authenticate(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	// TODO the actual authentication here, then ->
-	cookie, err := req.Cookie("session-id")
-	if err != nil {
-		id, _ := uuid.NewV4()
-		cookie = &http.Cookie{
-			Name:     "session-id",
-			Value:    id.String(),
-			HttpOnly: true,
-		}
-		http.SetCookie(res, cookie)
-	}
-	fmt.Println("Cookie", cookie)
-
+	createSessionCookie(res, req)
 	//NOTE example code to check if session exist and give back the user name if it does
 	// c.Value is the cookie value, so the userId
 	// if un, ok := dbSessions[c.Value]; ok {
@@ -45,5 +34,20 @@ func SignUp(res http.ResponseWriter, req *http.Request) {
 		fmt.Println("Create user failed with ", err)
 		http.Error(res, http.StatusText(500), http.StatusInternalServerError)
 	}
-	res.WriteHeader(http.StatusOK)
+	createSessionCookie(res, req)
+	// res.WriteHeader(http.StatusOK)
+}
+
+func createSessionCookie(res http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session-id")
+	if err != nil {
+		id, _ := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:     "session-id",
+			Value:    id.String(),
+			HttpOnly: true,
+		}
+		http.SetCookie(res, cookie)
+	}
+	fmt.Println("Cookie", cookie)
 }
