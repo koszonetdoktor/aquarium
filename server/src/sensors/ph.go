b732a63a576a0ca24ea47d2035ebfb3ad3c33c82
Gergo <halmi.gergo@gmail.com>
2019-09-15 10:50:47 +0200

    lint

--- server/src/sensors/ph.go
+++ server/src/sensors/ph.go
@@ -1,57 +1,54 @@
 package sensors
 
 import (
+	"config"
 	"fmt"
 	"log"
-	"time"
-	"config"
-	"github.com/MichaelS11/go-ads"
-	"periph.io/x/periph/conn/i2c"
-	"periph.io/x/periph/conn/i2c/i2creg"
-	"periph.io/x/periph/host"
 	"math"
+	"time"
+
+	"github.com/MichaelS11/go-ads"
 )
 
 func SamplingPh() {
-		err := ads.HostInit()
+	err := ads.HostInit()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var ads1 *ads.ADS
+	ads1, err = ads.NewADS("I2C1", 0x48, "")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ads1.SetConfigGain(ads.ConfigGain1)
+
+	for {
+		var result uint16
+		result, err = ads1.ReadRetry(5)
 		if err != nil {
-			fmt.Println(err)
+			ads1.Close()
+			fmt.Println("ERROR in Ph sampling", err)
 		}
-	
-		var ads1 *ads.ADS
-		ads1, err = ads.NewADS("I2C1", 0x48, "")
+
+		phRaw := caluclatePh(result)
+
+		ph := math.Floor(phRaw*100) / 100
+
+		err = config.Insert("water", "ph", ph)
 		if err != nil {
-			fmt.Println(err)
+			log.Println("ERROR: Saving the read ph!", err)
 		}
-	
-		ads1.SetConfigGain(ads.ConfigGain1)
-
-		for {
-			var result uint16
-			result, err = ads1.ReadRetry(5)
-			if err != nil {
-				ads1.Close()
-				fmt.Println("ERROR in Ph sampling", err)
-			}
-
-			phRaw := caluclatePh(result)
-
-			ph := math.Floor(phRaw*100)/100
-
-			err = config.Insert("water", "ph", ph)
-			if err != nil {
-				log.Println("ERROR: Saving the read ph!", err)
-			}
-			log.Println("Sample water ph: ", ph)
-			time.Sleep(1 * time.Minute)
-		}
+		log.Println("Sample water ph: ", ph)
+		time.Sleep(1 * time.Minute)
+	}
 }
-
 
 func caluclatePh(rawMeasurement uint16) float64 {
 	slope := 0.000488
 	intersection := 1.667
 
-	ph := slope * float64(rawMeasurement) + intersection
+	ph := slope*float64(rawMeasurement) + intersection
 	return ph
 }
